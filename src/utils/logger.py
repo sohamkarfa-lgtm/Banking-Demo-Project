48ae89b1be53b15e2a8fe5c420ef0e96fdbe7ba3
sohamkarfa-lgtm <soham.karfa@knowit.se>
2026-05-06 06:20:38 +0200

    Create logger.py

---
+++ src/utils/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.INFO)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
