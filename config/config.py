18c5f3c11f833bd895a14791cc26c301841869f6
sohamkarfa-lgtm <soham.karfa@knowit.se>
2026-05-06 06:08:24 +0200

    Delete config directory

--- config/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    def __init__(self):
-        self.catalog = "banking_catalog"
-        self.raw_volume = "/Volumes/banking_catalog/raw"
-        self.checkpoint_base = "/Volumes/banking_catalog/checkpoints"
