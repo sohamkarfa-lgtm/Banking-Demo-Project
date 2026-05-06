8a1b51bcae12d7bfbf486d8c25cdcdf2e6e66b7c
sohamkarfa-lgtm <soham.karfa@knowit.se>
2026-05-06 06:12:52 +0200

    Create bronze_pipeline.py

---
+++ src/pipelines/bronze_pipeline.py
@@ -0,0 +1,23 @@
+from .base_pipeline import BasePipeline
+
+class BronzePipeline(BasePipeline):
+    def __init__(self, spark, config, entity_config, reader, writer, path_utils, logger, monitoring):
+        super().__init__(spark, config, entity_config, logger, monitoring)
+        self.reader = reader
+        self.writer = writer
+        self.path_utils = path_utils
+
+    def run(self, entity):
+        try:
+            df = self.reader.read_stream(self.path_utils.raw_path(entity))
+
+            table = f"{self.config.catalog}.bronze.{entity}"
+            checkpoint = self.path_utils.checkpoint_path(entity, "bronze")
+
+            self.writer.write_stream(df, table, checkpoint)
+
+            self.monitoring.log(entity, "bronze", "SUCCESS")
+
+        except Exception as e:
+            self.monitoring.log(entity, "bronze", "FAILED", str(e))
+            raise
